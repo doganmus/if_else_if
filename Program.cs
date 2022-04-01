4a463a23c70cc9113afbaf1d309369a1a4cbf61c
Dogan Mus <doganmus87@gmail.com>
2022-04-01 23:09:48 +0300

    proje if_else_if sonucu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,25 @@ namespace if_else_if
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int time = DateTime.Now.Hour;
+
+            if (time>=18 && time<11)
+            {
+                Console.WriteLine("Günaydın");
+            }
+            else if(time<=18)
+            {
+               Console.WriteLine("İyi Günler"); 
+            }
+            else
+            {
+               Console.WriteLine("İyi geceler"); 
+            }
+
+            string sonuc= time<=18 ?"iyi günler":"iyi geceler";
+
+            sonuc = time>=6 && time<11 ?"Günaydı" : time<=18 ? "İyi günler" : "iyi geceler";
+            Console.WriteLine(sonuc);
         }
     }
 }
